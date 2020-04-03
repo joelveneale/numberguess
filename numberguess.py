410a6630ff83380a50271a023c0dc48b680a2b9d
joelveneale <joelpneale@gmail.com>
2020-04-03 18:22:02 +0100

    added a previous guess list function

--- numberguess.py
+++ numberguess.py
@@ -2,26 +2,35 @@ import random
 
 print("Welcome to GUESS THE NUMBER")
 guess_count = 10
+previous_guesses = []
 print("You have 10 Guesses. The number is between 1-100")
 random_number = random.randint(0, 100)
 while guess_count > 0:
     minmax = list(range(0, 100))
 
+
     guess1 = int(input("What is your guess?: "))
-
     if guess1 == random_number:
-        print("YOU GUESSED IT!")
-        guess_count = 0
+      print("YOU GUESSED IT!")
+      guess_count = 0
+    elif guess_count == 0:
+      print("You ran out of guesses!!")
     elif guess1 not in minmax:
-        print("Please input a number between 1-100.")
+      print("Please input a number between 1-100.")
     elif guess1 > random_number:
-        print("Try something a little lower...")
-        guess_count = guess_count - 1
+      print("Try something a little lower...")
+      guess_count = guess_count - 1
+      previous_guesses.append(guess1)
+      if previous_guesses:
+         print("Here are your previous guesses: " + str(previous_guesses))
     elif guess1 < random_number:
-        print("Try something a little higher...")
-        guess_count = guess_count - 1
+      print("Try something a little higher...")
+      guess_count = guess_count - 1
+      previous_guesses.append(guess1)
+      if previous_guesses:
+         print("Here are your previous guesses: " + str(previous_guesses))
     else:
-        print("Please input a number between 1-100.")
+       print("Please input a number between 1-100.")
 
-print("You ran out of guesses!!")
+print("Thanks for playing.")
 
