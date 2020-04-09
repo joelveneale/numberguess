8e2a22a2567fd1a5034997811110fe6ff44f1bb5
joelveneale <joelpneale@gmail.com>
2020-04-09 17:59:56 +0100

    updated functionality on previous guess function, still needs work on displaying the correct info after ech guess

--- numguessv2.py
+++ numguessv2.py
@@ -12,34 +12,27 @@ minmax = list(range(0, 100))
 victory = False
 
 def create_previous_guesses(guessInput):
-    previous_guesses['higher_guesses'].sort()
-    previous_guesses['lower_guesses'].sort()
-
     if guessInput > random_number:
         previous_guesses['higher_guesses'].append(guessInput)
         if previous_guesses['higher_guesses']:
           print("Try something a little lower...")
 
-        if previous_guesses['higher_guesses'] and previous_guesses['lower_guesses']:
-            print("Previous Higher Guesses: " + str(previous_guesses['higher_guesses']))
-            print("The number must be between " + (str(previous_guesses['lower_guesses'][-1]) + " and " + str(
-                previous_guesses['higher_guesses'][0]) + "..."))
-
-
     elif guessInput < random_number:
         previous_guesses['lower_guesses'].append(guessInput)
         if previous_guesses['lower_guesses']:
           print('Try something a little higher...')
-        if previous_guesses['higher_guesses'] and previous_guesses['lower_guesses']:
-            print("Previous Lower Guesses: " + str(previous_guesses['lower_guesses']))
-            print("The number must be between " + (str(previous_guesses['lower_guesses'][-1]) + " and " + str(
-                previous_guesses['higher_guesses'][0]) + "..."))
+
+
+    previous_guesses['higher_guesses'].sort()
+    previous_guesses['lower_guesses'].sort()
 
 def run_guessing_game():
     guess_count = 10
     while guess_count > 0 and victory == False:
         guessInput = check_if_guess_is_correct(int(input("What is your guess? ")))
         guess_count = guess_count - 1
+    if guess_count == 0:
+        print("You ran out of guesses!!")
     return
 
 def check_end_result():
@@ -52,12 +45,15 @@ def check_end_result():
 
 def check_if_guess_is_correct(guess1):
     create_previous_guesses(guess1)
+    if previous_guesses['higher_guesses'] and previous_guesses['lower_guesses']:
+        #print("Previous Lower Guesses: " + str(previous_guesses['lower_guesses']))
+        print("The number must be between " + (str(previous_guesses['lower_guesses'][-1]) + " and " + str(
+            previous_guesses['higher_guesses'][0]) + "..."))
     if guess1 == random_number:
       print("YOU GUESSED IT!")
       victory = True
 
-    #elif guess_count == 0:
-     # print("You ran out of guesses!!")
+
 
     elif guess1 not in minmax:
       print("Please input a number between 1-100.")
