0c75255dddd785e98352171c65b7b90917a5daa0
joelveneale <joelpneale@gmail.com>
2020-04-09 17:48:26 +0100

    updated functionality on previous guess function, still needs work on displaying the correct info after ech guess

---
+++ numguessv2.py
@@ -0,0 +1,73 @@
+import random
+from typing import Any, Union
+
+
+
+previous_guesses = {
+    'higher_guesses': [],
+    'lower_guesses': []
+}
+random_number = random.randint(0, 100)
+minmax = list(range(0, 100))
+victory = False
+
+def create_previous_guesses(guessInput):
+    previous_guesses['higher_guesses'].sort()
+    previous_guesses['lower_guesses'].sort()
+
+    if guessInput > random_number:
+        previous_guesses['higher_guesses'].append(guessInput)
+        if previous_guesses['higher_guesses']:
+          print("Try something a little lower...")
+
+        if previous_guesses['higher_guesses'] and previous_guesses['lower_guesses']:
+            print("Previous Higher Guesses: " + str(previous_guesses['higher_guesses']))
+            print("The number must be between " + (str(previous_guesses['lower_guesses'][-1]) + " and " + str(
+                previous_guesses['higher_guesses'][0]) + "..."))
+
+
+    elif guessInput < random_number:
+        previous_guesses['lower_guesses'].append(guessInput)
+        if previous_guesses['lower_guesses']:
+          print('Try something a little higher...')
+        if previous_guesses['higher_guesses'] and previous_guesses['lower_guesses']:
+            print("Previous Lower Guesses: " + str(previous_guesses['lower_guesses']))
+            print("The number must be between " + (str(previous_guesses['lower_guesses'][-1]) + " and " + str(
+                previous_guesses['higher_guesses'][0]) + "..."))
+
+def run_guessing_game():
+    guess_count = 10
+    while guess_count > 0 and victory == False:
+        guessInput = check_if_guess_is_correct(int(input("What is your guess? ")))
+        guess_count = guess_count - 1
+    return
+
+def check_end_result():
+    if victory == True:
+        print("Congratulations, you guessed the correct number in " + (10 - guess_count) + "guesses\n")
+    else:
+        print("Unlucky this time. You didn't guess the number within the guess count.\n")
+    print("Thanks for playing.")
+    return
+
+def check_if_guess_is_correct(guess1):
+    create_previous_guesses(guess1)
+    if guess1 == random_number:
+      print("YOU GUESSED IT!")
+      victory = True
+
+    #elif guess_count == 0:
+     # print("You ran out of guesses!!")
+
+    elif guess1 not in minmax:
+      print("Please input a number between 1-100.")
+    return
+
+def main():
+    print("Welcome to GUESS THE NUMBER")
+    run_guessing_game()
+    check_end_result()
+    return
+
+
+main()
