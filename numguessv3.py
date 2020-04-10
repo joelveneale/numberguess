b2a26a75c0daaad41479bf90387e2abcb0118338
joelveneale <joelpneale@gmail.com>
2020-04-10 10:32:32 +0100

    added a play again section

--- numguessv3.py
+++ numguessv3.py
@@ -46,6 +46,12 @@ def check_end_result(result):
     else:
         print("\nUnlucky this time. You didn't guess the number within the guess count.\n")
         print("The number was: " + str(random_number))
+        play_again = input('Do you want to play again? y/n...')
+        if play_again == 'y':
+            main()
+        else:
+            print("\nThanks for playing.\n")
+            sys.exit()
     print("\nThanks for playing.\n")
     sys.exit()
 
