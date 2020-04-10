f2fb4e3e15f6ce60d21d1ce0bc417bae41666780
Chris Laidler <cjjlaidler@gmail.com>
2020-04-10 12:25:53 +0100

    Fixed: play again option

--- numguessv3.py
+++ numguessv3.py
@@ -6,7 +6,6 @@ previous_guesses = {
     'higher': [],
     'lower': []
 }
-random_number = random.randint(0, 100)
 minmax = list(range(0, 100))
 
 def change_previous_guesses(guess, direction):
@@ -17,7 +16,7 @@ def change_previous_guesses(guess, direction):
     return
 
 
-def check_to_change_previous_guesses(guess):
+def check_to_change_previous_guesses(guess, random_number):
     if guess > random_number:
         change_previous_guesses(guess, 'higher')
 
@@ -28,17 +27,17 @@ def check_to_change_previous_guesses(guess):
     previous_guesses['higher'].sort()
 
 
-def run_guessing_game(guess_count, victory):
+def run_guessing_game(guess_count, victory, random_number):
     while guess_count > 0 and victory == False:
         victory = check_if_guess_is_correct(
-            int(input("What is your guess? ")), guess_count, victory)
+            int(input("What is your guess? ")), guess_count, victory, random_number)
         guess_count = guess_count - 1
     if guess_count == 0:
         print("You ran out of guesses!!")
     return {'guess_count': guess_count, 'victory': victory}
 
 
-def check_end_result(result):
+def check_end_result(result, random_number):
     guess_or_guesses = 'guess' if result['guess_count'] == 9 else 'guesses'
     if result['victory'] == True:
         print("\nCongratulations, you guessed the correct number in " +
@@ -57,8 +56,8 @@ def shall_we_play_again(play_again):
         sys.exit()
 
 
-def check_if_guess_is_correct(guess, guess_count, victory):
-    check_to_change_previous_guesses(guess)
+def check_if_guess_is_correct(guess, guess_count, victory, random_number):
+    check_to_change_previous_guesses(guess, random_number)
     if guess_count > 0 and previous_guesses['higher'] and previous_guesses['lower']:
         print("The number must be between " +
               (str(previous_guesses['lower'][-1]) + " and " + str(previous_guesses['higher'][0]) + "..."))
@@ -72,10 +71,11 @@ def check_if_guess_is_correct(guess, guess_count, victory):
 
 def main():
     print("Welcome to GUESS THE NUMBER\n")
+    random_number = random.randint(0, 100)
     guess_count = 10
     victory = False
-    result = run_guessing_game(guess_count, victory)
-    check_end_result(result)
+    result = run_guessing_game(guess_count, victory, random_number)
+    check_end_result(result, random_number)
     shall_we_play_again(input('Do you want to play again? y/n...'))
     return
 
