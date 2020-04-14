bbd5790afb62e6be44fb7ee9904b6164eed846ee
joelveneale <joelpneale@gmail.com>
2020-04-14 15:22:50 +0100

    joel v2 version
    added a difficulty selection that changes the amount of guesses and the range of the number.

--- numberguess_oop/main.py
+++ numberguess_oop/main.py
@@ -1,5 +1,6 @@
 from Models.Game import Game
 from Models.Guess import Guess
+from Models.Difficulty import Difficulty
 from Models.PreviousGuesses import PreviousGuesses
 from Models.Highscores import Highscores
 from lib.tinydb import TinyDB, Query
@@ -7,10 +8,11 @@ db = TinyDB('highscores.json')
 
 def main():
     print("\n--- Welcome to Guess the Number ---\n")
-    guess = Guess()
+    difficulty = Difficulty()
+    guess = Guess(difficulty)
     previousGuesses = PreviousGuesses()
     username = input("What is your username? ")
     highscores = Highscores(db, username)
-    game = Game(guess, previousGuesses, highscores)
+    game = Game(guess, previousGuesses, highscores, difficulty)
     game.run()
 main()
--- numguessv3.py
+++ numguessv3.py
@@ -6,53 +6,18 @@ previous_guesses = {
     'higher': [],
     'lower': []
 }
+random_number = random.randint(0, 100)
 minmax = list(range(0, 100))
 
-def main():
-    print("Welcome to GUESS THE NUMBER")
-    random_number = random.randint(0, 100)
-    guess_count = 10
-    victory = False
-    run_guessing_game(guess_count, victory, random_number)
+def change_previous_guesses(guess, direction):
+    opposite_direction = 'lower' if direction == 'higher' else 'higher'
+    previous_guesses[direction].append(guess)
+    if previous_guesses[direction]:
+        print("Try something a little " + opposite_direction + "...\n")
+    return
 
-def run_guessing_game(guess_count, victory, random_number):
-    while guess_count > 0 and victory == False:
-        victory = check_if_guess_is_correct(
-            validate_input("\nWhat is your guess? "), guess_count, victory, random_number)
-        if(victory != 'Failed input validation'):
-            guess_count = guess_count - 1
-        else:
-            victory = False
-    if guess_count == 0:
-        print("You ran out of guesses!!")
-    check_end_result({'guess_count': guess_count, 'victory': victory}, random_number)
 
-def validate_input(input_message):
-    guess = input(input_message)
-    if guess is "":
-        print('Oops. You forgot to input a value. Please try again.')
-        return False
-    if guess.isdigit():
-        return int(guess)
-    else:
-        print("Oops you didn't input a number. Please try again.")
-        return False
-
-def check_if_guess_is_correct(guess, guess_count, victory, random_number):
-    if guess == False:
-        return 'Failed input validation'
-    check_to_change_previous_guesses(guess, random_number)
-    if guess_count > 0 and previous_guesses['higher'] and previous_guesses['lower']:
-        print("The number must be between " +
-              (str(previous_guesses['lower'][-1]) + " and " + str(previous_guesses['higher'][0]) + "..."))
-    if guess == random_number:
-        return True
-
-    elif guess not in minmax:
-        print("Please input a number between 1-100.")
-    return False
-    
-def check_to_change_previous_guesses(guess, random_number):
+def check_to_change_previous_guesses(guess):
     if guess > random_number:
         change_previous_guesses(guess, 'higher')
 
@@ -62,14 +27,18 @@ def check_to_change_previous_guesses(guess, random_number):
     previous_guesses['lower'].sort()
     previous_guesses['higher'].sort()
 
-def change_previous_guesses(guess, direction):
-    opposite_direction = 'lower' if direction == 'higher' else 'higher'
-    previous_guesses[direction].append(guess)
-    if previous_guesses[direction]:
-        print("Try something a little " + opposite_direction + "...")
-    return
 
-def check_end_result(result, random_number):
+def run_guessing_game(guess_count, victory):
+    while guess_count > 0 and victory == False:
+        victory = check_if_guess_is_correct(
+            int(input("What is your guess? ")), guess_count, victory)
+        guess_count = guess_count - 1
+    if guess_count == 0:
+        print("You ran out of guesses!!")
+    return {'guess_count': guess_count, 'victory': victory}
+
+
+def check_end_result(result):
     guess_or_guesses = 'guess' if result['guess_count'] == 9 else 'guesses'
     if result['victory'] == True:
         print("\nCongratulations, you guessed the correct number in " +
@@ -77,13 +46,38 @@ def check_end_result(result, random_number):
     else:
         print("\nUnlucky this time. You didn't guess the number within the guess count.\n")
         print("The number was: " + str(random_number))
-    shall_we_play_again(input('Do you want to play again? y/n...'))
+
+
 
 def shall_we_play_again(play_again):
     if play_again == 'y':
         main()
     else:
         print("\nThanks for playing.\n")
-        sys.exit()    
+        sys.exit()
+
+
+def check_if_guess_is_correct(guess, guess_count, victory):
+    check_to_change_previous_guesses(guess)
+    if guess_count > 0 and previous_guesses['higher'] and previous_guesses['lower']:
+        print("The number must be between " +
+              (str(previous_guesses['lower'][-1]) + " and " + str(previous_guesses['higher'][0]) + "..."))
+    if guess == random_number:
+        return True
+
+    elif guess not in minmax:
+        print("Please input a number between 1-100.")
+    return False
+
+
+def main():
+    print("Welcome to GUESS THE NUMBER\n")
+    guess_count = 10
+    victory = False
+    result = run_guessing_game(guess_count, victory)
+    check_end_result(result)
+    shall_we_play_again(input('Do you want to play again? y/n...'))
+    return
+
 
 main()
