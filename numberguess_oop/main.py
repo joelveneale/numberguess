c4ea6e29353e82cc1e87f3248d467dab26bb4aaa
joelveneale <joelpneale@gmail.com>
2020-04-15 09:29:22 +0100

    Updated highscores to include a difficulty level

--- numberguess_oop/main.py
+++ numberguess_oop/main.py
@@ -1,9 +1,10 @@
-from Models.Game import Game
-from Models.Guess import Guess
-from Models.Difficulty import Difficulty
-from Models.PreviousGuesses import PreviousGuesses
-from Models.Highscores import Highscores
-from lib.tinydb import TinyDB, Query
+from numberguess_oop.Models.Game import Game
+from numberguess_oop.Models.Guess import Guess
+from numberguess_oop.Models.Difficulty import Difficulty
+from numberguess_oop.Models.PreviousGuesses import PreviousGuesses
+from numberguess_oop.Models.Highscores import Highscores
+from numberguess_oop.lib.tinydb import TinyDB
+
 db = TinyDB('highscores.json')
 
 def main():
@@ -12,7 +13,8 @@ def main():
     guess = Guess(difficulty)
     previousGuesses = PreviousGuesses()
     username = input("What is your username? ")
-    highscores = Highscores(db, username)
+    level = difficulty.level
+    highscores = Highscores(db, username, level)
     game = Game(guess, previousGuesses, highscores, difficulty)
     game.run()
 main()
